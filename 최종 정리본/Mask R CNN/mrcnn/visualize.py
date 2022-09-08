9fc45271d30e61ec34f4a8ddef8f24f9a01ddb44
toast-ceo <70907737+toast-ceo@users.noreply.github.com>
2022-09-08 23:01:20 +0900

    save

--- 최종 정리본/Mask R CNN/mrcnn/visualize.py
+++ 최종 정리본/Mask R CNN/mrcnn/visualize.py
@@ -393,7 +393,8 @@ def display_instances_predict_whitemask( file_name, image, boxes, masks, class_i
     # 경로설정
     # temp_DIR = 'C:/Users/user/Desktop/LeeSac/Mask_RCNN/datasets/crosswalk/learning_datasets_inside_predict_mask/'
     # temp_DIR = 'C:/Users/user/Desktop/LeeSac/Mask_RCNN/datasets/crosswalk/learning_datasets_outside_predict_mask/'
-    temp_DIR = 'C:/Users/user/Desktop/LeeSac/Mask_RCNN/datasets/crosswalk_v2/testset_predict_mask/'
+#     temp_DIR = 'C:/Users/user/Desktop/LeeSac/Mask_RCNN/datasets/crosswalk_v2/testset_predict_mask/'
+    temp_DIR = 'C:/Users/user/Desktop/temp_img/'
 
    
     
